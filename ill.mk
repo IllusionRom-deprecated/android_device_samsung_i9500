a978119cf8192fbbd278778068f14305554dc362
AdityaSureka <adityasureka92@gmail.com>
2014-03-20 10:07:18 -0400

    Illusionify

--- ill.mk
+++ ill.mk
@@ -1,15 +1,15 @@
 $(call inherit-product, device/samsung/i9500/full_i9500.mk)
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/illusion/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/illusion/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/illusion/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_i9500
+PRODUCT_NAME := ill_i9500
 PRODUCT_DEVICE := i9500
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
